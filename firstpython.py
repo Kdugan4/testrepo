46f77c839a627d447785012e4e33bec8ed6b0fd2
Kdugan4 <109085107+Kdugan4@users.noreply.github.com>
2023-11-27 10:02:56 -0500

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,2 +1,237 @@
 #Display the output
 print("New Python File")
+
+## Lab 1: Collecting the Data
+# Requests allow us to make HTTP requests which we will use to get data from an API
+import requests
+# Pandas is a software library written for the Python programming language for data manipulation and analysis.
+import pandas as pd
+# NumPy is a library for the Python programming language, adding support for large, multi-dimensional arrays and matrices, along with a large collection of high-level mathematical functions to operate on these arrays
+import numpy as np
+# Datetime is a library that allows us to represent dates
+import datetime
+
+# Setting this option will print all columns of a dataframe
+pd.set_option('display.max_columns', None)
+# Setting this option will print all of the data in a feature
+pd.set_option('display.max_colwidth', None)
+
+# Takes the dataset and uses the rocket column to call the API and append the data to the list
+def getBoosterVersion(data):
+    for x in data['rocket']:
+       if x:
+        response = requests.get("https://api.spacexdata.com/v4/rockets/"+str(x)).json()
+        BoosterVersion.append(response['name'])
+
+# Takes the dataset and uses the launchpad column to call the API and append the data to the list
+def getLaunchSite(data):
+    for x in data['launchpad']:
+       if x:
+         response = requests.get("https://api.spacexdata.com/v4/launchpads/"+str(x)).json()
+         Longitude.append(response['longitude'])
+         Latitude.append(response['latitude'])
+         LaunchSite.append(response['name'])
+
+# Takes the dataset and uses the payloads column to call the API and append the data to the lists
+def getPayloadData(data):
+    for load in data['payloads']:
+       if load:
+        response = requests.get("https://api.spacexdata.com/v4/payloads/"+load).json()
+        PayloadMass.append(response['mass_kg'])
+        Orbit.append(response['orbit'])
+
+# Takes the dataset and uses the cores column to call the API and append the data to the lists
+def getCoreData(data):
+    for core in data['cores']:
+            if core['core'] != None:
+                response = requests.get("https://api.spacexdata.com/v4/cores/"+core['core']).json()
+                Block.append(response['block'])
+                ReusedCount.append(response['reuse_count'])
+                Serial.append(response['serial'])
+            else:
+                Block.append(None)
+                ReusedCount.append(None)
+                Serial.append(None)
+            Outcome.append(str(core['landing_success'])+' '+str(core['landing_type']))
+            Flights.append(core['flight'])
+            GridFins.append(core['gridfins'])
+            Reused.append(core['reused'])
+            Legs.append(core['legs'])
+            LandingPad.append(core['landpad'])
+
+spacex_url="https://api.spacexdata.com/v4/launches/past"
+response = requests.get(spacex_url)
+print(response.content)
+
+#TASK 1: Request and parse the SpaceX launch data using the GET request
+#To make the requested JSON results more consistent we will use the following static response object for this project:
+static_json_url='https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/datasets/API_call_spacex_api.json'
+#The 200 status code tells us the request was successful.
+response.status_code
+#Now we decode the response contaent as Json using .json() and turn it into a Padnads dataframe useing .json_normalize()
+data=pd.json_normalize(response.json())
+data.head()
+
+#alot of the data are IDs. I.e. rocket has no data just IDs. We will use the API to get info about the launches using the IDs.
+# Lets take a subset of our dataframe keeping only the features we want and the flight number, and date_utc.
+data = data[['rocket', 'payloads', 'launchpad', 'cores', 'flight_number', 'date_utc']]
+
+# We will remove rows with multiple cores because those are falcon rockets with 2 extra rocket boosters and rows that have multiple payloads in a single rocket.
+data = data[data['cores'].map(len)==1]
+data = data[data['payloads'].map(len)==1]
+
+# Since payloads and cores are lists of size 1 we will also extract the single value in the list and replace the feature.
+data['cores'] = data['cores'].map(lambda x : x[0])
+data['payloads'] = data['payloads'].map(lambda x : x[0])
+
+# We also want to convert the date_utc to a datetime datatype and then extracting the date leaving the time
+data['date'] = pd.to_datetime(data['date_utc']).dt.date
+
+# Using the date we will restrict the dates of the launches
+data = data[data['date'] <= datetime.date(2020, 11, 13)]
+
+#Global variables 
+BoosterVersion = []
+PayloadMass = []
+Orbit = []
+LaunchSite = []
+Outcome = []
+Flights = []
+GridFins = []
+Reused = []
+Legs = []
+LandingPad = []
+Block = []
+ReusedCount = []
+Serial = []
+Longitude = []
+Latitude = []
+
+BoosterVersion
+
+getBoosterVersion (data)
+getLaunchSite(data)
+getPayloadData(data)
+getCoreData(data)
+
+#Finally lets construct our dataset using the data we have obtained. We combine the columns into a dictionary
+
+launch_dict = {'FlightNumber': list(data['flight_number']),
+'Date': list(data['date']),
+'BoosterVersion':BoosterVersion,
+'PayloadMass':PayloadMass,
+'Orbit':Orbit,
+'LaunchSite':LaunchSite,
+'Outcome':Outcome,
+'Flights':Flights,
+'GridFins':GridFins,
+'Reused':Reused,
+'Legs':Legs,
+'LandingPad':LandingPad,
+'Block':Block,
+'ReusedCount':ReusedCount,
+'Serial':Serial,
+'Longitude': Longitude,
+'Latitude': Latitude}
+
+data=pd.DataFrame(launch_dict)
+data.head()
+data.describe()
+
+#Task 2: Filter the dataframe to only include Falcon 9 launches
+data_falcon9=data[data['BoosterVersion']=='Falcon 9']
+data_falcon9
+
+data_falcon9.loc[:, 'FlightNumber']= list(range(1, data_falcon9.shape[0]+1))
+data_falcon9
+# We have missing values
+data_falcon9.isnull().sum()
+
+#Task 3 Dealing with Missing Values
+# Caluculate the mean value of Payload Mass
+mean= data_falcon9['PayloadMass'].mean()
+#Replace the np.nan values with the mean.
+data_falcon9['PayloadMass'].replace([np.nan], mean, inplace=True)
+data_falcon9.isnull().sum()
+
+
+##Lab 2: Webscraping
+#import packages
+!pip3 install beautifulsoup4
+!pip3 install requests
+import sys
+
+import requests
+from bs4 import BeautifulSoup
+import re
+import unicodedata
+import pandas as pd
+
+#Helper functions
+def date_time(table_cells):
+    """
+    This function returns the data and time from the HTML  table cell
+    Input: the  element of a table data cell extracts extra row
+    """
+    return [data_time.strip() for data_time in list(table_cells.strings)][0:2]
+
+def booster_version(table_cells):
+    """
+    This function returns the booster version from the HTML  table cell 
+    Input: the  element of a table data cell extracts extra row
+    """
+    out=''.join([booster_version for i,booster_version in enumerate( table_cells.strings) if i%2==0][0:-1])
+    return out
+
+def landing_status(table_cells):
+    """
+    This function returns the landing status from the HTML table cell 
+    Input: the  element of a table data cell extracts extra row
+    """
+    out=[i for i in table_cells.strings][0]
+    return out
+
+
+def get_mass(table_cells):
+    mass=unicodedata.normalize("NFKD", table_cells.text).strip()
+    if mass:
+        mass.find("kg")
+        new_mass=mass[0:mass.find("kg")+2]
+    else:
+        new_mass=0
+    return new_mass
+
+
+def extract_column_from_header(row):
+    """
+    This function returns the landing status from the HTML table cell 
+    Input: the  element of a table data cell extracts extra row
+    """
+    if (row.br):
+        row.br.extract()
+    if row.a:
+        row.a.extract()
+    if row.sup:
+        row.sup.extract()
+        
+    colunm_name = ' '.join(row.contents)
+    
+    # Filter the digit and empty names
+    if not(colunm_name.strip().isdigit()):
+        colunm_name = colunm_name.strip()
+        return colunm_name    
+
+static_url = "https://en.wikipedia.org/w/index.php?title=List_of_Falcon_9_and_Falcon_Heavy_launches&oldid=1027686922"
+
+for row in soup.find("tbody").find_all('tr'):
+    col = row.find_all("td")
+    FlightNo = col[0].text
+    DateTime = col[1].text
+    LaunchSite = col[3].text
+    Payload = col[4].text
+    PayloadMass = col[5].text
+    Orbit = col[6].text
+    Customer= col[7].text
+    LaunchOutcome= col[8].text
+
+data = data.append({"Flight No.":FlightNo, "Date and time ( )":DateTime, "BoosterVersion":BoosterVersion, "Launch site":LaunchSite, "Payload":Payload, "Payload mass":PayloadMass, "Orbit":Orbit, "Customer": Customer, "Launch outcome": LaunchOutcomes, "BoosterLandings": BoosterLandings}, ignore_index=True)  
